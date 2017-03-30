1a18e4fae2eceee0fef46b772baea2d138c8c4d9
dimian13 <dimian13@gmail.com>
2017-03-30 22:19:06 +0300

    Добавлен @RequestMapping для запросов /insert, /update, /delete

--- src/main/java/io/delivery/controller/AppController.java
+++ src/main/java/io/delivery/controller/AppController.java
@@ -1,8 +1,6 @@
 package io.delivery.controller;
 
-import io.delivery.model.Answer;
-import io.delivery.model.Message;
-import io.delivery.model.TableCreator;
+import io.delivery.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +14,12 @@ public class AppController {
     private Message message;
     @Autowired
     private TableCreator tableCreator;
+    @Autowired
+    private TableInsertor tableInsertor;
+    @Autowired
+    private TableUpdater tableUpdater;
+    @Autowired
+    private TableDeletor tableDeletor;
 
     @RequestMapping("/")
     public String hello(Model model) {
@@ -28,5 +32,26 @@ public class AppController {
     public String create(Model model){
         model.addAttribute("status", tableCreator.createCompany());
         return "create";
+    }
+
+    @RequestMapping("/insert")
+    public String insert(Model model){
+        model.addAttribute("insert", tableInsertor.insertNewDataIntoTable("insert into " +
+                "companies (code, title, did) values(11111, 'Title_1', 10)"));
+        return "insert";
+    }
+
+    @RequestMapping("/update")
+    public String update(Model model){
+        model.addAttribute("update", tableUpdater.updateDataInTable("update companies " +
+                "set code='22222', title='Title_2', did='22' " +
+                "where code='11111'"));
+        return "update";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(Model model){
+        model.addAttribute("delete", tableDeletor.deleteExistingTable());
+        return "delete";
     }
 }
