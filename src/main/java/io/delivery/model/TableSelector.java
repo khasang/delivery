24c3556b74792d1f6ab54ef6c0a6e4ed830afa6c
dimian13 <dimian13@gmail.com>
2017-03-30 23:10:32 +0300

    Добавлен класс TableSelector выбирающий все строки из таблицы users по id=6 с помощью sql запроса

--- src/main/java/io/delivery/model/TableSelector.java
+++ src/main/java/io/delivery/model/TableSelector.java
@@ -1,7 +1,19 @@
 package io.delivery.model;
 
-/**
- * Created by fixer on 30.03.2017.
- */
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class TableSelector {
+    private JdbcTemplate jdbcTemplate;
+
+    public TableSelector(){}
+
+    public TableSelector(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
+
+    public String selectDataFromTable(){
+        String query = "SELECT * FROM users WHERE ID='6'";
+        jdbcTemplate.execute(query);
+        return "Data selected";
+    }
 }
