518e486162bb68ea811dcd3703eab5cfd78aa157
ksereda <alternativer2005@yandex.ru>
2017-04-05 16:55:11 +0300

    add security crypto pass to basic structure

--- src/main/java/io/delivery/config/SecurityConfig.java
+++ src/main/java/io/delivery/config/SecurityConfig.java
@@ -1,14 +1,32 @@
 package io.delivery.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication (AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -21,12 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .and().csrf().disable().formLogin().defaultSuccessUrl("/", false);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
-        auth.inMemoryAuthentication().withUser("user2").password("user2").roles("USER2");
-        auth.inMemoryAuthentication().withUser("user3").password("user3").roles("USER3");
-        auth.inMemoryAuthentication().withUser("user4").password("user4").roles("USER4");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+//        auth.inMemoryAuthentication().withUser("user2").password("user2").roles("USER2");
+//        auth.inMemoryAuthentication().withUser("user3").password("user3").roles("USER3");
+//        auth.inMemoryAuthentication().withUser("user4").password("user4").roles("USER4");
+//    }
 }
