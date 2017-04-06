b5c64261d61c78d5a0fb6fcf3b8e452cabe8c8d3
Ayrat <ayrat1@mail.ru>
2017-04-06 22:50:00 +0300

    log4j & spring security complete

--- src/main/java/io/delivery/config/SecurityConfig.java
+++ src/main/java/io/delivery/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package io.delivery.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +18,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/secure").access("hasRole('ADMIN')")
+                .antMatchers("/secure", "/create", "/insert", "/update", "/delete")
+                .access("hasRole('ADMIN')")
                 .and().csrf().disable().formLogin().defaultSuccessUrl("/", false);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//    }
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean (name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/io/delivery/controller/AppController.java
+++ src/main/java/io/delivery/controller/AppController.java
@@ -2,9 +2,13 @@ package io.delivery.controller;
 
 import io.delivery.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -29,25 +33,25 @@ public class AppController {
     }
 
     @RequestMapping("/create")
-    public String create(Model model){
+    public String create(Model model) {
         model.addAttribute("status", tableCreator.createCompany());
         return "create";
     }
 
     @RequestMapping("/insert")
-    public String insert(Model model){
+    public String insert(Model model) {
         model.addAttribute("insert", tableInserter.insertValue());
         return "insert";
     }
 
     @RequestMapping("/update")
-    public String update(Model model){
+    public String update(Model model) {
         model.addAttribute("update", tableUpdater.updateValue());
         return "update";
     }
 
     @RequestMapping("/delete")
-    public String delete(Model model){
+    public String delete(Model model) {
         model.addAttribute("delete", tableDeleter.deleteValue());
         return "delete";
     }
@@ -56,4 +60,13 @@ public class AppController {
     public String secure() {
         return "/secure";
     }
+
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
+    }
+
 }
