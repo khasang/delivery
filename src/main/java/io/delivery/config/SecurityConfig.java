033f83a3cb999ad6e8580fcbf20d986633d6dc1f
dimian13 <dimian13@gmail.com>
2017-04-09 00:21:26 +0300

    added security config

--- src/main/java/io/delivery/config/SecurityConfig.java
+++ src/main/java/io/delivery/config/SecurityConfig.java
@@ -1,27 +1,43 @@
 package io.delivery.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/secure").access("hasRole('ADMIN')")
-                .antMatchers("/valera").access("hasRole('VALERA')")
+                .antMatchers("/delete").access("hasRole('ADMIN')")
+                .antMatchers("/create").access("hasRole('TRUMP')")
+                .antMatchers("/insert").access("hasRole('TRUMP')")
+                .antMatchers("/update").access("hasRole('TRUMP')")
+                .antMatchers("/select").access("hasRole('DOG')")
+                .antMatchers("/backup").access("hasRole('DOG')")
                 .and().csrf().disable().formLogin().defaultSuccessUrl("/", false);
 
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("valera").password("valera").roles("VALERA");
-    }
 }
