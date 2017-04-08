13083d97847e095c48b22614d4c61049ea11ddba
dimian13 <dimian13@gmail.com>
2017-04-09 00:19:40 +0300

    added view of crypted password at /password/ page

--- src/main/java/io/delivery/controller/AppController.java
+++ src/main/java/io/delivery/controller/AppController.java
@@ -2,9 +2,13 @@ package io.delivery.controller;
 
 import io.delivery.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -63,13 +67,12 @@ public class AppController {
         return "secure";
     }
 
-    @RequestMapping("/vasya")
-    public String secureVasya(){
-        return "secure";
+    @RequestMapping(value ={"/password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
     }
 
-    @RequestMapping("/valera")
-    public String secureValera(){
-        return "valera";
-    }
 }
