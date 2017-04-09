cf93cec866b0012681ae29a7c02f35fce7f42f53
ksereda <alternativer2005@yandex.ru>
2017-04-09 19:40:56 +0300

    add Hibernate settings to basic structure

---
+++ src/main/java/io/delivery/entity/Document.java
@@ -0,0 +1,34 @@
+package io.delivery.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "documents")
+public class Document {
+    @Id
+    @Column(name = "document_id")
+    private long id;
+//    @Column(name = "document_name")
+    private String name;
+    @Transient
+    private String specificInnetInfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Document() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
