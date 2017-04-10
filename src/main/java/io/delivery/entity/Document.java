779bf60560776f42cce58900c17d0cfa2d2d6d96
Yuri <alekseenkoyuri1989@gmail.com>
2017-04-10 18:43:48 +0300

    add crud and add spring security with db

---
+++ src/main/java/io/delivery/entity/Document.java
@@ -0,0 +1,43 @@
+package io.delivery.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "document")
+public class Document {
+    @Id
+    @Column(name = "document_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "document_name")
+    private String name;
+    @Transient
+    private String specificInnerInfo;
+
+    public Document() {
+    }
+
+    public String getSpecificInnerInfo() {
+        return specificInnerInfo;
+    }
+
+    public void setSpecificInnerInfo(String specificInnerInfo) {
+        this.specificInnerInfo = specificInnerInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
