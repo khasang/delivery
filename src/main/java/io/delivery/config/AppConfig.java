6372bb308e9a5c53c02021d1604d3f275903bc6e
ATugunov <tgnv@mail.ru>
2017-04-10 15:40:20 +0300

    Added simple Hibernate "document" entity

--- src/main/java/io/delivery/config/AppConfig.java
+++ src/main/java/io/delivery/config/AppConfig.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
