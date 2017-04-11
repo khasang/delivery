55bd8f1aebb580245f33bdc0ba310db026102ba0
dimian13 <dimian13@gmail.com>
2017-04-12 01:59:11 +0300

    add basic dao level working with database

---
+++ src/main/java/io/delivery/dao/BasicDao.java
@@ -0,0 +1,30 @@
+package io.delivery.dao;
+
+import org.hibernate.Session;
+
+import java.util.List;
+
+/**
+* Basic methods for DAO
+*/
+public interface BasicDao<T> {
+
+    /**
+     * @return current Hibernate session
+     */
+    Session getCurrentSession();
+
+    /**
+     * @return list for entity
+     */
+    List<T> getList();
+
+    /**
+     * Create entity at database
+     *
+     * @param entity - current entity
+     * @return created entity
+     */
+    T create(T entity);
+
+}
