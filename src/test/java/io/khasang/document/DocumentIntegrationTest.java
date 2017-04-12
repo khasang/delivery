b98c6cbbcc9a8444316c4af2ab84453d6e0bea41
ksereda <alternativer2005@yandex.ru>
2017-04-12 16:53:21 +0300

    add test 2 to Hibernate structure

--- src/test/java/io/khasang/document/DocumentIntegrationTest.java
+++ src/test/java/io/khasang/document/DocumentIntegrationTest.java
@@ -2,11 +2,12 @@ package io.khasang.document;
 
 
 import io.delivery.entity.Document;
+import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.*;
 
 public class DocumentIntegrationTest {
     private final String ROOT = "http://localhost:8080/document";
@@ -20,6 +21,20 @@ public class DocumentIntegrationTest {
     @Test
     public void addDocument() {
         Document document = createDocument();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Document> responseEntity = restTemplate.exchange(
+                ROOT + GET_ID + "{id}",
+                HttpMethod.GET,
+                null,
+                Document.class,
+                document.getId()
+        );
+
+        Document resultDocument = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(resultDocument);
     }
 
     private Document createDocument() {
@@ -29,7 +44,15 @@ public class DocumentIntegrationTest {
         Document document = prefillDocument();
         HttpEntity<Document> httpEntity = new HttpEntity<>(document, headers);
         RestTemplate restTemplate = new RestTemplate();
-        Document createdDocument = restTemplate.exchange()
+        Document createdDocument = restTemplate.exchange(
+                ROOT+ADD,
+                HttpMethod.POST,
+                httpEntity,
+                Document.class
+        ).getBody();
+        assertNotNull(createdDocument);
+        assertEquals(document.getName(), createdDocument.getName());
+        return createdDocument;
     }
 
     private Document prefillDocument() {
