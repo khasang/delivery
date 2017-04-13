e0affc9cb42bc9a03de415a70d0f8752eb645f40
ksereda <alternativer2005@yandex.ru>
2017-04-13 12:56:14 +0300

    add test full to Hibernate structure

--- src/test/java/io/khasang/document/DocumentIntegrationTest.java
+++ src/test/java/io/khasang/document/DocumentIntegrationTest.java
@@ -85,52 +85,52 @@ public class DocumentIntegrationTest {
     @Test
     public void deleteDocument(){
         Document document = createDocument();
-        assertNotNull(document);
+        assertNotNull(document);  // проверяем документ на 0
 
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();   // для работы с REST-сервисами
         ResponseEntity<String> responseEntity = restTemplate.exchange(
-                ROOT + DELETE + "{id}",
-                HttpMethod.DELETE,
-                null,
-                String.class,
-                document.getId()
+                ROOT + DELETE + "{id}",  // корневой путь, куда делается запрос
+                HttpMethod.DELETE,   // тип запроса
+                null,  // передаём тело запроса (0 т.к. ID)
+                String.class,  // к чему должны привести ответ (работаем как с классом Стринга)
+                document.getId() // доп параметр (кидаем в документ Id - изменчивая часть)
         );
 
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());  // сравниваем статус с ответом
 
-        ResponseEntity<Document> checkDocumentById = restTemplate.exchange(
-                ROOT + GET_ID + "{id}",
-                HttpMethod.GET,
-                null,
-                Document.class,
-                document.getId()
+        ResponseEntity<Document> checkDocumentById = restTemplate.exchange(   // проверка что  документа с таким Id НЕ СУЩЕСТВУЕТ !  (checkDocumentById)
+                ROOT + GET_ID + "{id}",  // корневой путь, куда делается запрос
+                HttpMethod.GET,  // тип запроса
+                null,  // передаём тело запроса (0 т.к. ID)
+                Document.class,  // к чему должны привести ответ (работаем как с классом Стринга)
+                document.getId()  // доп параметр (кидаем в документ Id - изменчивая часть)
         );
 
-        assertEquals(HttpStatus.OK, checkDocumentById.getStatusCode());
-        assertNull(checkDocumentById.getBody());
+        assertEquals(HttpStatus.OK, checkDocumentById.getStatusCode());  // сравниваем статус с Документом на рповерку Id
+        assertNull(checkDocumentById.getBody());  // проверка на 0 (наш документ который мы проверяем на не существующий Id)
     }
 
     @Test
     public void updateDocument(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpHeaders headers = new HttpHeaders();   // для типа запроса (Content-Type)
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);  // тип запроса APPLICATION_JSON_UTF8
 
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();  // для работы с REST сервисами
         Document document = createDocument();
-        assertNotNull(document);
+        assertNotNull(document);  // проверка на создание документа (не равен 0)
 
         document.setName("Sword");
 
         HttpEntity<Document> httpEntity = new HttpEntity<>(document, headers);
-        Document resultUpdate = restTemplate.exchange(
+        Document resultUpdate = restTemplate.exchange(   // создаём наш документ resultUpdate
                 ROOT + UPDATE,
                 HttpMethod.PUT,
                 httpEntity,
                 Document.class
         ).getBody();
 
-        assertNotNull(resultUpdate);
-        assertNotNull(resultUpdate.getId());
-        assertEquals("Sword", resultUpdate.getName());
+        assertNotNull(resultUpdate);  // проверка на 0 нашего документа resultUpdate
+        assertNotNull(resultUpdate.getId());  // проверка на 0 по Id нашего документа resultUpdate
+        assertEquals("Sword", resultUpdate.getName());  // проверка, что "Sword" должен соответствовать имени нашего документа resultUpdate
     }
 }
