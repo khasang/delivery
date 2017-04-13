383ae4e7c8261dace8bcd74fe7979b61c2c52c11
dimian13 <dimian13@gmail.com>
2017-04-13 18:09:33 +0300

    add CustomerDao interface to work with database

---
+++ src/main/java/io/delivery/dao/CustomerDao.java
@@ -0,0 +1,35 @@
+package io.delivery.dao;
+
+import io.delivery.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDao extends BasicDao<Customer>{
+    /**
+     * Find customers by name in database
+     * @param name - customer's name
+     * @return - list of customers
+     */
+    List<Customer> findCustomerByName(String name);
+
+    /**
+     * Find customers by address in database
+     * @param address - customer's address
+     * @return - list of customers, if more then one customer registered at same address
+     */
+    List<Customer> findCustomerByAddress(String address);
+
+    /**
+     * Find customer by phone number
+     * @param phoneNumber - customer's phone number
+     * @return - current customer
+     */
+    Customer findCustomerByPhoneNumber(String phoneNumber);
+
+    /**
+     * Find customer by e-mail
+     * @param eMail - customer's e-mail
+     * @return - current customer
+     */
+    Customer findCustomerByEmail(String eMail);
+}
