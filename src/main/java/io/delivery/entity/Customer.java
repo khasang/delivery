0c384918dd2fe8627eae0a9150e6bd918164de93
dimian13 <dimian13@gmail.com>
2017-04-13 17:55:04 +0300

    add Customer entity

--- src/main/java/io/delivery/entity/Customer.java
+++ src/main/java/io/delivery/entity/Customer.java
@@ -1,7 +1,59 @@
 package io.delivery.entity;
 
-/**
- * Created by fixer on 13.04.2017.
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @Column
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String address;
+    @Column(name = "telephone_number")
+    private String telephoneNumber;
+    @Column(name = "e-mail")
+    private String eMail;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
 }
