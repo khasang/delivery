d6cc63c2a11efd41e2721bddb163a836cce920e3
dimian13 <dimian13@gmail.com>
2017-04-14 22:56:10 +0300

    changed @RequstMapping from "/customers" to "/customer"

--- src/main/java/io/delivery/controller/CustomerController.java
+++ src/main/java/io/delivery/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/customers")
+@RequestMapping("/customer")
 public class CustomerController {
 
     final private CustomerService customerService;
@@ -28,13 +28,13 @@ public class CustomerController {
 
     @RequestMapping(value = "/get/eMail/{eMail}", method = RequestMethod.GET)
     @ResponseBody
-    public List<Customer> getCustomerByEmail(@PathVariable(value = "eMail") String eMail){
+    public Customer getCustomerByEmail(@PathVariable(value = "eMail") String eMail){
         return customerService.findCustomerByEmail(eMail);
     }
 
     @RequestMapping(value = "/get/phoneNumber/{phoneNumber}", method = RequestMethod.GET)
     @ResponseBody
-    public List<Customer> getCustomerByPhoneNumber(@PathVariable(value = "phoneNumber") String phoneNumber){
+    public Customer getCustomerByPhoneNumber(@PathVariable(value = "phoneNumber") String phoneNumber){
         return customerService.findCustomerByPhoneNumber(phoneNumber);
     }
 
