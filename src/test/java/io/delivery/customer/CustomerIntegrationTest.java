4e5678a1ba82f170d660edd1f23e8b50a420f02c
ksereda <alternativer2005@yandex.ru>
2017-04-18 22:27:20 +0300

    add all Integration Tests

--- src/test/java/io/delivery/customer/CustomerIntegrationTest.java
+++ src/test/java/io/delivery/customer/CustomerIntegrationTest.java
@@ -1,23 +1,19 @@
 package io.delivery.customer;
 
 import io.delivery.entity.Customer;
-import io.delivery.service.CustomerService;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-
 public class CustomerIntegrationTest {
-//    @Autowired
-//    private CustomerService customerService;
 
     private final String ROOT = "http://localhost:8080/customer";
-    private final String GET_MAIL = "/get_by/e-mail/";
     private final String GET_PHONE = "/get_by/phone-number/";
     private final String GET_ADDRESS = "/get_by/address/";
     private final String GET_ID = "/get/id/";
@@ -26,27 +22,7 @@ public class CustomerIntegrationTest {
     private final String DELETE = "/delete_by/id/";
     private final String ALL = "/get/all";
     private final String GET_NAME = "/get_by/name/";
-
-
-//    @Test
-//    public void addDocumentAndGet() {              // тест на добавление документа
-//        Customer customer = createCustomer();
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        ResponseEntity<Customer> responseEntity = restTemplate.exchange(
-//                ROOT + GET_ID + "{id}",
-//                HttpMethod.GET,
-//                null,
-//                Customer.class,
-//                customer.getId()
-//        );
-//
-//        Customer resultCustomer = responseEntity.getBody();
-//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-//        assertNotNull(resultCustomer);
-//    }
-
+    private final String GET_EMAIL = "/get_by/e-mail/";
 
     @Test
     public void getCustomerByName() {
@@ -56,7 +32,8 @@ public class CustomerIntegrationTest {
                 ROOT + GET_NAME + "{name}",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<Customer>>() {},
+                new ParameterizedTypeReference<List<Customer>>() {
+                },
                 customer.getName()
         );
 
@@ -66,7 +43,6 @@ public class CustomerIntegrationTest {
         assertNotNull(list);
         assertNotNull(list.get(0));
     }
-
 
     private Customer createCustomer() {
         HttpHeaders headers = new HttpHeaders();
@@ -88,11 +64,6 @@ public class CustomerIntegrationTest {
 
     private Customer prefillCustomer() {
         Customer customer = new Customer();
-//        customerService.createCustomer(customer);
-//        customer.getName();
-//        customer.getAddress();
-//        customer.geteMail();
-//        customer.getPhoneNumber();
         customer.setAddress("minsk");
         customer.seteMail("1@gmail.com");
         customer.setPhoneNumber("2345639");
@@ -100,4 +71,63 @@ public class CustomerIntegrationTest {
         return customer;
     }
 
+    @Test
+    public void getCustomerByAddress() {
+        Customer customer = createCustomer();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Customer>> responseEntity = restTemplate.exchange(
+                ROOT + GET_ADDRESS + "{address}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Customer>>() {
+                },
+                customer.getAddress()
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        List<Customer> list2 = responseEntity.getBody();
+        assertNotNull(list2);
+        assertNotNull(list2.get(0));
+    }
+
+    @Test
+    public void getCustomerByPhone() {
+        Customer customer = createCustomer();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Customer>> responseEntity = restTemplate.exchange(
+                ROOT + GET_PHONE + "{phoneNumber}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Customer>>() {
+                },
+                customer.getPhoneNumber()
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        List<Customer> list3 = responseEntity.getBody();
+        assertNotNull(list3);
+        assertNotNull(list3.get(0));
+    }
+
+    @Test
+    public void getCustomerByEmail() {
+        Customer customer = createCustomer();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Customer>> responseEntity = restTemplate.exchange(
+                ROOT + GET_EMAIL + "{eMail}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Customer>>() {
+                },
+                customer.geteMail()
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        List<Customer> list4 = responseEntity.getBody();
+        assertNotNull(list4);
+        assertNotNull(list4.get(0));
+    }
 }
