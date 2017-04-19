e7522b762c99cfd9abdd39646f75ecffbb8bc3ee
ksereda <alternativer2005@yandex.ru>
2017-04-19 15:55:23 +0300

    add mockito library to pom.xml file and add mockito test findCustomerByName

---
+++ src/test/java/io/delivery/customer/CustomerMockTest.java
@@ -0,0 +1,20 @@
+package io.delivery.customer;
+
+import io.delivery.entity.Customer;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.BDDMockito;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+public class CustomerMockTest {
+
+    @Test
+    public void testFindCustomerByName() {
+        Customer customer = mock(Customer.class);
+        given(customer.getName()).willReturn("Ivan");
+        Assert.assertEquals(customer.getName(), "Ivan");
+    }
+}
