0e873b0040a04155a3f192c8ab226ab08ec614d4
aliasxrus <aliasxrus@gmail.com>
2017-04-22 23:40:23 +0500

    add OfficeServiceImplTest, DocumentIntegrationTest, OfficeControllerTest

--- src/main/java/io/delivery/dao/impl/OfficeDaoImpl.java
+++ src/main/java/io/delivery/dao/impl/OfficeDaoImpl.java
@@ -1,14 +1,8 @@
 package io.delivery.dao.impl;
 
 import io.delivery.dao.OfficeDao;
-import io.delivery.entity.Document;
 import io.delivery.entity.Office;
 
-import java.util.List;
-
-/**
- * Created by NortT on 16.04.2017.
- */
 public class OfficeDaoImpl extends BasicDaoImpl<Office> implements OfficeDao{
     public OfficeDaoImpl(Class<Office> entityClass) {
         super(entityClass);
--- src/test/java/io/delivery/controller/OfficeControllerTest.java
+++ src/test/java/io/delivery/controller/OfficeControllerTest.java
@@ -1,20 +1,38 @@
 package io.delivery.controller;
 
+import io.delivery.entity.Office;
+import io.delivery.service.impl.OfficeServiceImpl;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
 
 public class OfficeControllerTest {
     @Test
     public void addOffice() throws Exception {
+        OfficeServiceImpl officeService = mock(OfficeServiceImpl.class);
+        Office office = new Office();
+        given(officeService.create(office)).willReturn(office);
+        Assert.assertEquals(officeService.create(office), office);
     }
 
     @Test
     public void deleteOffice() throws Exception {
+        OfficeServiceImpl officeService = mock(OfficeServiceImpl.class);
+        Office office = new Office();
+        given(officeService.delete(1L)).willReturn(office);
+        Assert.assertEquals(officeService.delete(1L), office);
     }
 
     @Test
     public void getOfficeList() throws Exception {
+        OfficeServiceImpl officeService = mock(OfficeServiceImpl.class);
+        List<Office> list = new ArrayList<>();
+        given(officeService.getOfficeList()).willReturn(list);
+        Assert.assertEquals(officeService.getOfficeList(), list);
     }
-
 }
