f2c2ef22a37ed61b266e0c6e298e5a0a71fbf7a7
habuvo <ilya@canto.ru>
2017-04-26 17:04:56 +0300

    SOAP service

---
+++ src/main/java/io/delivery/service/Client.java
@@ -0,0 +1,32 @@
+package io.delivery.service;
+
+import net.webservicex.*;
+
+public class Client {
+
+    private CurrencyConvertorSoap convertor = new CurrencyConvertor().getCurrencyConvertorSoap();
+    private CountrySoap countryDetails = new Country().getCountrySoap();
+
+    public Client() {
+    }
+
+    public String currencyInfo(String country) {
+      return countryDetails.getCurrencyByCountry(country);
+    }
+
+    public String currencyRate(String from, String to) {
+        Double currencyRate = convertor.conversionRate(Currency.RUB,Currency.USD);
+        return currencyRate.toString();
+    }
+
+    public String getCurrencyName(String result) {
+        if (result.contains("Table")) {
+            return result.substring(result.indexOf("<Currency>")+10,result.indexOf("</Currency>"));
+        }
+        return "unknown";
+    }
+
+    public String getCurrencyCode(String result) {
+        return result.substring(result.indexOf("<CurrencyCode>")+14,result.indexOf("</CurrencyCode>"));
+    }
+}
---
+++ web/WEB-INF/views/country.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>CountryDetails</title>
+</head>
+<body>
+<p>Currency of ${name} is ${currency}</p>
+Actual exchange rate to RUB is ${rate}
+</body>
+</html>
