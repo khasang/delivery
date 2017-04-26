cc8634dc04ef7560bfab66d26b89dc70f07e8985
vitkulov <ratrex@mail.ru>
2017-04-26 19:44:42 +0300

    add parsing

--- src/main/java/io/delivery/controller/AppController.java
+++ src/main/java/io/delivery/controller/AppController.java
@@ -93,10 +93,5 @@ public class AppController {
         modelAndView.addObject("info", client.result(check));
         return modelAndView;
     }
-
-    @RequestMapping(value = "/country")
-    public String country() {
-        return "country";
-    }
 }
 
--- src/main/java/net/webservicex/CountryClient.java
+++ src/main/java/net/webservicex/CountryClient.java
@@ -1,5 +1,9 @@
 package net.webservicex;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class CountryClient {
 
     public CountryClient() {
@@ -8,7 +12,15 @@ public class CountryClient {
     public String getCountryByCode(String code) {
         Country countryService = new Country();
         CountrySoap country = countryService.getCountrySoap();
-        String result = country.getCountryByCountryCode(code);
-        return result;
+        String preResult = country.getCountryByCountryCode(code);
+        return parseCountry(preResult);
+    }
+
+    private String parseCountry(String preResult) {
+        Document document;
+        document = Jsoup.parse(preResult);
+        Elements codes = document.select("countrycode");
+        Elements names = document.select("name");
+        return "Code: " + codes.get(0).text() + "<br/>" + "Name: " + names.get(0).text();
     }
 }
