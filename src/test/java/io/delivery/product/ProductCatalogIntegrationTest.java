dfb0a378c458d55b153c36932f4c57c9656d720a
ATugunov <tgnv@mail.ru>
2017-04-27 16:52:44 +0300

    Continuation of Product Catalog integration tests development. Round 2.

--- src/test/java/io/delivery/product/ProductCatalogIntegrationTest.java
+++ src/test/java/io/delivery/product/ProductCatalogIntegrationTest.java
@@ -11,13 +11,17 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -29,31 +33,42 @@ public class ProductCatalogIntegrationTest {
     @Autowired
     private ProductService productService;
 
-    private ProductCatalogSection createdCatalogSection = new ProductCatalogSection();
+    private List<ProductCatalogSection> createdCatalogSections = new ArrayList<>();
+    private ProductCatalogSection createdSection = new ProductCatalogSection();
     private Product createdProduct = new Product();
     private ProductImage createdImage = new ProductImage();
 
 
     @Before
     public void createTestEntities() {
-        createdCatalogSection.setName("PizzaTest");
-        productService.addCatalogSection(createdCatalogSection);
+        createdSection.setName("PizzaTest");
+        productService.addCatalogSection(createdSection);
 
         createdProduct.setName("Magic");
         createdProduct.setDescription("fire");
         createdProduct.setPrice(100);
-        createdProduct.setProductCatalogSection(createdCatalogSection);
+        createdProduct.setProductCatalogSection(createdSection);
         productService.addProduct(createdProduct);
 
         createdImage.setImage(new byte[] {1, 2 , 3, 4});
         createdImage.setProduct(createdProduct);
         productService.addImage(createdImage);
+
+        List<ProductImage> imageList = new ArrayList<>();
+        imageList.add(createdImage);
+        createdProduct.setImages(imageList);
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(createdProduct);
+        createdSection.setProducts(productList);
+
+        createdCatalogSections.add(createdSection);
     }
 
     @After
     public void deleteTestEntities() {
-        if (createdCatalogSection != null) {
-            productService.deleteCatalogSection(createdCatalogSection.getId());
+        for (ProductCatalogSection section : createdCatalogSections) {
+            productService.deleteCatalogSection(section.getId());
         }
     }
 
@@ -65,7 +80,7 @@ public class ProductCatalogIntegrationTest {
         product.setName("Magic");
         product.setDescription("fire");
         product.setPrice(100);
-        product.setProductCatalogSection(createdCatalogSection);
+        product.setProductCatalogSection(createdCatalogSections.get(0));
         HttpEntity<Product> httpEntity = new HttpEntity<>(product, headers);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<Product> responseEntity = restTemplate.exchange(
@@ -95,6 +110,24 @@ public class ProductCatalogIntegrationTest {
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertNotNull(resultProduct);
         assertEquals(createdProduct.getName(), resultProduct.getName());
+    }
+
+    @Test
+    public void getProductsByCatalogSectionId() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<List<Product>> result = restTemplate.exchange(
+                ROOT + "/getProductsByCatalogSectionId/" + "{id}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Product>>() {
+                },
+                createdSection.getId()
+        );
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertNotNull(result.getBody());
+        List<Product> list = result.getBody();
+        assertNotNull(list.get(0));
     }
 
     @Test
@@ -197,5 +230,223 @@ public class ProductCatalogIntegrationTest {
 
         assertEquals(HttpStatus.OK, checkProductById.getStatusCode());
         assertNull(checkProductById.getBody());
+    }
+
+    @Test
+    public void getAllCatalogSections() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<List<ProductCatalogSection>> result = restTemplate.exchange(
+                ROOT + "/getAllCatalogSections",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ProductCatalogSection>>() {
+                }
+        );
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertNotNull(result.getBody());
+        List<ProductCatalogSection> list = result.getBody();
+        assertNotNull(list.get(0));
+    }
+
+    @Test
+    public void addCatalogSection() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        ProductCatalogSection section = new ProductCatalogSection();
+        section.setName("Magic");
+        HttpEntity<ProductCatalogSection> httpEntity = new HttpEntity<>(section, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ProductCatalogSection> responseEntity = restTemplate.exchange(
+                ROOT + "/addCatalogSection",
+                HttpMethod.POST,
+                httpEntity,
+                ProductCatalogSection.class
+        );
+        ProductCatalogSection addedSection = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(addedSection);
+        assertEquals(addedSection.getName(), section.getName());
+        createdCatalogSections.add(addedSection);
+    }
+
+    @Test
+    public void updateCatalogSection() {
+        createdSection.setName("Sword");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<ProductCatalogSection> httpEntity = new HttpEntity<>(createdSection, headers);
+        ResponseEntity<ProductCatalogSection> responseEntity = restTemplate.exchange(
+                ROOT + "/updateCatalogSection",
+                HttpMethod.PUT,
+                httpEntity,
+                ProductCatalogSection.class
+        );
+        ProductCatalogSection updatedSection = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(updatedSection);
+        assertEquals(updatedSection.getName(), createdSection.getName());
+    }
+
+    @Test
+    public void deleteCatalogSection(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                ROOT + "/deleteCatalogSection/" + "{id}",
+                HttpMethod.DELETE,
+                null,
+                String.class,
+                createdSection.getId()
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        ResponseEntity<Product> checkSectionById = restTemplate.exchange(
+                ROOT + "/getCatalogSectionById/" + "{id}",
+                HttpMethod.GET,
+                null,
+                Product.class,
+                createdSection.getId()
+        );
+
+        assertEquals(HttpStatus.OK, checkSectionById.getStatusCode());
+        assertNull(checkSectionById.getBody());
+
+        createdCatalogSections.remove(createdSection);
+    }
+
+    @Test
+    public void getCatalogSectionById() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<ProductCatalogSection> responseEntity = restTemplate.exchange(
+                ROOT + "/getCatalogSectionById/" + "{id}",
+                HttpMethod.GET,
+                null,
+                ProductCatalogSection.class,
+                createdSection.getId()
+        );
+        ProductCatalogSection resultSection = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(resultSection);
+        assertEquals(createdSection.getName(), resultSection.getName());
+    }
+
+    @Test
+    public void getImageIdsByProductId() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<List<Long>> result = restTemplate.exchange(
+                ROOT + "/getImageIdsByProductId/" + "{id}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Long>>() {
+                },
+                createdProduct.getId()
+        );
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertNotNull(result.getBody());
+        List<Long> list = result.getBody();
+        assertNotNull(list.get(0));
+        assertEquals((long)list.get(0), createdImage.getId());
+    }
+
+    @Test
+    public void deleteImage(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                ROOT + "/deleteImage/" + "{id}",
+                HttpMethod.DELETE,
+                null,
+                String.class,
+                createdImage.getId()
+        );
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        ResponseEntity<List<Long>> result = restTemplate.exchange(
+                ROOT + "/getImageIdsByProductId/" + "{id}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Long>>() {
+                },
+                createdProduct.getId()
+        );
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        List<Long> list = result.getBody();
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void getImageById() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<byte[]> responseEntity = restTemplate.exchange(
+                ROOT + "/getImageById/" + "{id}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<byte[]>() {
+                },
+                createdImage.getId()
+        );
+        byte[] result = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertArrayEquals(result, createdImage.getImage());
+    }
+
+    @Test
+    public void addImage() {
+        class ImageFileResource extends ByteArrayResource {
+            private final String filename;
+
+            private ImageFileResource(final byte[] byteArray, final String filename) {
+                super(byteArray);
+                this.filename = filename;
+            }
+
+            @Override
+            public String getFilename() {
+                return filename;
+            }
+        }
+
+        MultiValueMap<String, Object> data = new LinkedMultiValueMap<>();
+        data.add("file", new ImageFileResource(createdImage.getImage(), "upload.jpeg"));
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(data);
+        ResponseEntity<Map<String,String>> response = getRestTemplate().exchange(url, HttpMethod.POST, requestEntity, new ParameterizedTypeReference<Map<String,String>>() {});
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<byte[]> responseEntity = restTemplate.exchange(
+                ROOT + "/uploadProductImage/" + "{id}",
+                HttpMethod.POST,
+                null,
+                new ParameterizedTypeReference<byte[]>() {
+                },
+                createdImage.getId()
+        );
+        byte[] result = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertArrayEquals(result, createdImage.getImage());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        ProductImage image = new ProductImage();
+        image.setImage(new byte[] {0, 1, 2, 3, 4});
+        HttpEntity<ProductCatalogSection> httpEntity = new HttpEntity<>(section, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ProductCatalogSection> responseEntity = restTemplate.exchange(
+                ROOT + "/addCatalogSection",
+                HttpMethod.POST,
+                httpEntity,
+                ProductCatalogSection.class
+        );
+        ProductCatalogSection addedSection = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(addedSection);
+        assertEquals(addedSection.getName(), section.getName());
+
     }
 }
