55883608b378a303408a524d17b4924fe6e55d76
ATugunov <tgnv@mail.ru>
2017-07-03 16:48:55 +0300

    Creating ProductCatalog UI with jQuery. Round 9. Finish of insertion of product catalog section. Start of edition of product catalog section.

--- web/WEB-INF/views/js/product-catalog-sections-crud.js
+++ web/WEB-INF/views/js/product-catalog-sections-crud.js
@@ -1,59 +1,84 @@
 $(document).ready(function () {
-    var addProductCatalogSectionDialog;
-    var serverInteractionAlarm;
+    var add_modifySectionDialog;
     var loadPanel;
+    var modificationMode = {
+        addition : {value: 0},
+        edition: {value: 1}
+    };
+    var currentSectionModificationMode;
 
     function showCatalogSections () {
-        $.getJSON("/products/getAllCatalogSections", function (resp) {
-            var sectionsHtml = "";
-            $.each( resp, function( key, value ) {
-                sectionsHtml += "<tr>";
-                sectionsHtml += "<td class=\"delivery-invisible\">";
-                sectionsHtml += value.id;
-                sectionsHtml += "</td>";
-                sectionsHtml += "<td>";
-                sectionsHtml += value.name;
-                sectionsHtml += "</td>";
-                sectionsHtml += "</tr>";
+        $("#loadPanel").dxLoadPanel("show");
+        $.ajax("/products/getAllCatalogSections", {
+            method: "GET",
+            dataType: "json",
+            timeout: 10000,
+            contentType: "application/json; charset=UTF-8",
+        })
+            .done(function (resp) {
+                var sectionsHtml = "";
+                $.each( resp, function( key, value ) {
+                    sectionsHtml += "<tr>";
+                    sectionsHtml += "<td class=\"delivery-invisible\">";
+                    sectionsHtml += value.id;
+                    sectionsHtml += "</td>";
+                    sectionsHtml += "<td>";
+                    sectionsHtml += value.name;
+                    sectionsHtml += "</td>";
+                    sectionsHtml += "</tr>";
+                });
+                var sectionsElement = $("#sections");
+                sectionsElement.empty();
+                sectionsElement.html(sectionsHtml);
+                $("#loadPanel").dxLoadPanel("hide");
+            })
+            .fail(function (jqXHR, textStatus, errorThrown ) {
+                $("#loadPanel").dxLoadPanel("hide");
+                if (textStatus === "timeout") {
+                    DevExpress.ui.notify("Нет ответа от сервера", "error");
+                }
+                else {
+                    DevExpress.ui.notify("Ошибка сервера: " + jqXHR.status + " " + errorThrown, "error");
+                }
             });
-            var sectionsElement = $("#sections");
-            sectionsElement.empty();
-            sectionsElement.html(sectionsHtml);
-        });
     }
 
     function addSection() {
-        var sectionName = $("#addSectionForm input[name='name']")[0].value;
+        var sectionName = $("#sectionNameFormInput")[0].value;
         var json;
         if (sectionName.length !== 0) {
-            json = $("#addSectionForm").serializeJSON();
-            $.ajax("/products/addCatalogSectionError", {
+            json = {
+                name: sectionName
+            };
+            $.ajax("/products/addCatalogSection", {
                 method: "POST",
                 dataType: "json",
-                data: json,
+                data: JSON.stringify(json),
                 timeout: 10000,
                 contentType: "application/json; charset=UTF-8",
-                success: function (data, textStatus, jqXHR) {
-                    addProductCatalogSectionDialog.dialog("close");
-                    showCatalogSections();
-                },
-                error: function (jqXHR, textStatus, errorThrown ) {
-                    if (textStatus === "timeout") {
-                        $("#serverInteractionAlarm p").text("Нет ответа от сервера");
-                        serverInteractionAlarm.dialog("open");
-                    }
-                    else {
-                        $("#serverInteractionAlarm p").text(errorThrown.toString());
-                        serverInteractionAlarm.dialog("open");
-                    }
+            })
+            .done(function () {
+                add_modifySectionDialog.dialog("close");
+                showCatalogSections();
+            })
+            .fail(function (jqXHR, textStatus, errorThrown ) {
+                if (textStatus === "timeout") {
+                    DevExpress.ui.notify("Нет ответа от сервера", "error");
+                }
+                else {
+                    DevExpress.ui.notify("Ошибка сервера: " + jqXHR.status + " " + errorThrown, "error");
                 }
             });
+        }
+        else {
+            $("#sectionNameFormGroup").addClass("has-error");
+            DevExpress.ui.notify("Наименование раздела каталога не может быть пустым", "error", 2000);
         }
     }
 
     $("tbody#sections").selectable({
         stop: function () {
-            var numSelected = $(".ui-selected", this).length;
+            var numSelected = $(".ui-selected").length;
             if (numSelected > 1) {
                 $("#editButton").prop("disabled", true);
                 $("#deleteButton").prop("disabled", false);
@@ -71,41 +96,38 @@ $(document).ready(function () {
         }
     });
 
-    addProductCatalogSectionDialog = $("#addSectionDialog").dialog({
+    add_modifySectionDialog = $("#add-modifySectionDialog").dialog({
         autoOpen: false,
         modal: true,
         title: "Добавление нового раздела",
+        closeText: "Отмена",
         buttons: {
             "Создать": addSection,
             "Отмена": function() {
-                addProductCatalogSectionDialog.dialog("close")
+                add_modifySectionDialog.dialog("close")
             }
         },
         close: function() {
-            $("#addSectionForm")[0].reset();
-            $("#sectionName").removeClass("ui-state-error");
-        }
-    });
-
-    serverInteractionAlarm = $("#serverInteractionAlarm").dialog({
-        autoOpen: false,
-        modal: true,
-        title: "Ошибка взаимодействия с сервером",
-        buttons: {
-            "Отмена": function() {
-                serverInteractionAlarm.dialog("close")
-            }
+            $("#add-modifySectionForm")[0].reset();
+            $("#sectionNameFormGroup").removeClass("has-error");
         }
     });
 
     $("#addButton").on("click", function () {
-        addProductCatalogSectionDialog.dialog("open");
-        showCatalogSections();
+        currentSectionModificationMode = modificationMode.addition;
+        add_modifySectionDialog.dialog.options("title", "Добавление нового раздела");
+        add_modifySectionDialog.dialog("open");
     });
 
-    $("#addSectionForm").on("submit", function(event) {
+    $("#add-modifySectionForm").on("submit", function(event) {
         event.preventDefault();
-        addSection();
+        if (currentSectionModificationMode === modificationMode.addition) {
+            addSection();
+        }
+    });
+
+    $("#sectionNameFormInput").on("input", function () {
+        $("#sectionNameFormGroup").removeClass("has-error");
     });
 
     loadPanel = $("#loadPanel").dxLoadPanel({
@@ -116,15 +138,7 @@ $(document).ready(function () {
         showPane: true,
         shading: true,
         closeOnOutsideClick: false
-        // onShown: function(){
-        //     setTimeout(function () {
-        //         loadPanel.hide();
-        //     }, 3000);
-        // },
-        // onHidden: function(){
-        //     showEmployeeInfo(employee);
-        // }
-    }).dxLoadPanel("instance");
+    });
 
     showCatalogSections();
 });
--- web/WEB-INF/views/productCatalog.jsp
+++ web/WEB-INF/views/productCatalog.jsp
@@ -3,16 +3,16 @@
 <html>
     <head>
         <title>Delivery - Разделы каталога продуктов</title>
-        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script type="text/javascript" src="https://cdn3.devexpress.com/jslib/17.1.4/js/dx.all.js"></script>
+        <script type="text/javascript" src="js/dx.all.js"></script>
         <script type="text/javascript" src="js/product-catalog-sections-crud.js"></script>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-        <link rel="stylesheet" type="text/css" href="https://cdn3.devexpress.com/jslib/17.1.4/css/dx.common.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdn3.devexpress.com/jslib/17.1.4/css/dx.light.css" />
+        <link rel="stylesheet" type="text/css" href="css/dx.common.css" />
+        <link rel="stylesheet" type="text/css" href="css/dx.light.css" />
         <link rel="stylesheet" type="text/css" href="css/delivery.css" />
 
     </head>
@@ -20,20 +20,14 @@
     <body>
 
     <div class="container-fluid">
-        <div id="addSectionDialog">
-            <form id="addSectionForm">
-                <div class="form-group">
-                    <label for="addSectionFormNameInput">Раздел</label>
-                    <input type="text" id="addSectionFormNameInput" name="name" class="form-control" >
+        <div id="add-modifySectionDialog">
+            <form id="add-modifySectionForm">
+                <div id="sectionNameFormGroup" class="form-group">
+                    <label class="control-label" for="sectionNameFormInput">Раздел</label>
+                    <input type="text" id="sectionNameFormInput" name="name" class="form-control">
                 </div>
+                <input type="hidden" id="sectionIdFormInput" name="id">
             </form>
-        </div>
-
-        <div id="serverInteractionAlarm">
-            <div class="ui-state-error">
-                <span class="ui-icon ui-icon-alert"></span>
-                <p>Server error</p>
-            </div>
         </div>
 
         <div id="loadPanel"></div>
